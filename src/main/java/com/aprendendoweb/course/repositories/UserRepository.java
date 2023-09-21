4d9a351d4c58a29900478d8d7ba5ac6afe08734f
Joice Lima <steffanyjoice006@gmail.com>
2023-09-21 14:41:15 -0300

    Component registration

--- src/main/java/com/aprendendoweb/course/repositories/UserRepository.java
+++ src/main/java/com/aprendendoweb/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aprendendoweb.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	
